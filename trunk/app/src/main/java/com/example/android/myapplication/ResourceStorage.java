29b8601991a7869e2dcf8e90b0167390f3e4e12a
andrey.kelasev@gmail.com <andrey.kelasev@gmail.com@49e85729-9275-97aa-b58e-e21b176551c3>
2014-12-26 03:14:08 +0000

    Create skeleton

--- trunk/app/src/main/java/com/example/android/myapplication/ResourceStorage.java
+++ trunk/app/src/main/java/com/example/android/myapplication/ResourceStorage.java
@@ -14,6 +14,40 @@ public class ResourceStorage extends Activity {
         HashMap<String,String> imageNamesMap = new HashMap<String, String>();
         imageNamesMap.put(String.valueOf(R.drawable.nevermind),"nevermind");
         imageNamesMap.put(String.valueOf(R.drawable.pink_floyd),"pink floyd");
+        imageNamesMap.put(String.valueOf(R.drawable.abbey_road),"abbey road");
+        imageNamesMap.put(String.valueOf(R.drawable.american_idiot),"american idiot");
+        imageNamesMap.put(String.valueOf(R.drawable.animals),"animals");
+        imageNamesMap.put(String.valueOf(R.drawable.back_in_black),"back in black");
+        imageNamesMap.put(String.valueOf(R.drawable.kid_a),"kid a");
+        imageNamesMap.put(String.valueOf(R.drawable.mothership),"mothership");
+        imageNamesMap.put(String.valueOf(R.drawable.radioactivity),"radioactivity");
+        imageNamesMap.put(String.valueOf(R.drawable.rage_against_the_machine),"rage against the machine");
+        imageNamesMap.put(String.valueOf(R.drawable.sgt_peppers_lonely_hearts_club),"sgt peppers lonely hearts club");
+        imageNamesMap.put(String.valueOf(R.drawable.unknown_pleasures),"unknown pleasures");
+        imageNamesMap.put(String.valueOf(R.drawable.velvet_underground),"velvet underground");
+        imageNamesMap.put(String.valueOf(R.drawable.war),"war");
+
+        return imageNamesMap;
+    }
+
+    public HashMap<String,String> createNotificationMap(){
+        HashMap<String,String> imageNamesMap = new HashMap<String, String>();
+        imageNamesMap.put(String.valueOf(R.drawable.nevermind),"The baby boy on the cover of ‘Nevermind’ was almost a girl\n"+
+                "‘Nevermind’ cover photographer Kirk Weddle did two shoots and really liked a photograph he took with a baby girl, but the band's record label insisted: we want the d--k.");
+        imageNamesMap.put(String.valueOf(R.drawable.pink_floyd),"The music and lyrics for the entire album were written during a seven week period in which the band were preparing for a tour in which they desperately wanted to premier new material.");
+        imageNamesMap.put(String.valueOf(R.drawable.abbey_road),"The album's working title was 'Everest,' an inside joke about the brand of cigarettes smoked by engineer Geoff Emerick. They had intended to shoot the cover photo at Mount Everest, but none of them wanted to make the long journey. Instead, McCartney suggested naming it after the street on which EMI Studios was located. That meant they could photograph themselves in the nearest crosswalk, which took about 30 minutes total.\n");
+        imageNamesMap.put(String.valueOf(R.drawable.american_idiot),"Armstrong wrote a few songs for \"Idiot\" while walking around the track at Macalester College in St. Paul. His wife, Adrienne, is from the Twin Cities, and they have a home here.");
+        imageNamesMap.put(String.valueOf(R.drawable.animals),"animals");
+        imageNamesMap.put(String.valueOf(R.drawable.back_in_black),"In fact, Back in Black has sold in excess of 50 million copies worldwide, making it one of the best selling albums in the history of music, only second to Micheal Jackson's Thriller.");
+        imageNamesMap.put(String.valueOf(R.drawable.kid_a),"Thom was in a band called Headless Chickens. His first band with Colin Greenwood was called TNT.");
+        imageNamesMap.put(String.valueOf(R.drawable.mothership),"mothership");
+        imageNamesMap.put(String.valueOf(R.drawable.radioactivity),"The song plays upon the meaning of its title, with the line \"Discovered by Madame Curie\" juxtaposed with \"Tune in to the melody\". ");
+        imageNamesMap.put(String.valueOf(R.drawable.rage_against_the_machine),"At Woodstock '99, Commerford draped the American flag over his bass amp and lit it on fire. He says he celebrating his right to free speech.");
+        imageNamesMap.put(String.valueOf(R.drawable.sgt_peppers_lonely_hearts_club),"Lennon wanted Jesus, Gandhi, and Hitler on the cover. He had recently taken a lot of heat for saying The Beatles were \"Bigger than Jesus,\" so they decided not to. (thanks, don - rapid city, SD, for above 2)");
+        imageNamesMap.put(String.valueOf(R.drawable.unknown_pleasures),"The 39 minutes of Joy Division's debut album Unknown Pleasures changed music forever: a mixture of Ian Curtis' mesmerising vocals and lyrics, Bernard Sumner's spiky guitar work, Peter Hook's distinctive bass playing and Stephen Morris' hypnotic drum patterns, all brought together by the mad genius that was producer Martin Hannett.");
+        imageNamesMap.put(String.valueOf(R.drawable.velvet_underground),"Lou Reed's father was a tax accountant, who raised him with the expectation that he would take over the family business. When little Lou turned out to be more of a handful than they'd bargained for, his parents signed their son over to the Creedmore State Psychiatric Hospital, where Lou Reed suffered eight weeks of electroshock therapy at the age of 17.");
+        imageNamesMap.put(String.valueOf(R.drawable.war)," This release, in 1991,was the first occurrence of its kind since the release of 'Help' by the Beatles in 1965.");
+
         return imageNamesMap;
     }
 
--- trunk/app/src/main/java/com/example/android/myapplication/ViewStorage.java
+++ trunk/app/src/main/java/com/example/android/myapplication/ViewStorage.java
@@ -1,8 +1,5 @@
 package com.example.android.myapplication;
 
-import android.app.Activity;
-import android.content.res.Resources;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TableRow;
@@ -16,11 +13,10 @@ import java.util.ArrayList;
 public class ViewStorage /*extends Activity*/{
     MyActivity activity;
     TextView tvName;
-    ImageButton btnName1;
-    ImageButton btnName2;
-    ImageButton btnName3;
-    ImageButton btnName4;
-    ImageButton btnName5;
+    ImageButton btnName1;ImageButton btnName2;ImageButton btnName3;ImageButton btnName4;ImageButton btnName5;
+    ImageButton btnName6;ImageButton btnName7;ImageButton btnName8;ImageButton btnName9;ImageButton btnName10;
+    ImageButton btnName11;ImageButton btnName12;ImageButton btnName13;ImageButton btnName14;ImageButton btnName15;
+
     TableRow.LayoutParams rw = new TableRow.LayoutParams(225,225);
 
     ArrayList<?super View> listViews = new ArrayList<View>();
@@ -30,26 +26,82 @@ public class ViewStorage /*extends Activity*/{
     }
 
         public void  addViews(){
-        btnName1 = (ImageButton) activity.findViewById(R.id.imageButton11);
-        btnName1.setBackgroundResource(R.drawable.nevermind);
-        btnName1.setLayoutParams(rw);
+            btnName1 = (ImageButton) activity.findViewById(R.id.imageButton11);
+            btnName1.setBackgroundResource(R.drawable.nevermind);
+            btnName1.setLayoutParams(rw);
 
-        btnName2 = (ImageButton) activity.findViewById(R.id.imageButton12);
-        btnName2.setBackgroundResource(R.drawable.nevermind);
-        btnName2.setLayoutParams(rw);
+            btnName2 = (ImageButton) activity.findViewById(R.id.imageButton12);
+            btnName2.setBackgroundResource(R.drawable.pink_floyd);
+            btnName2.setLayoutParams(rw);
 
-        btnName3 = (ImageButton) activity.findViewById(R.id.imageButton13);
-        btnName3.setBackgroundResource(R.drawable.nevermind);
-        btnName3.setLayoutParams(rw);
+            btnName3 = (ImageButton) activity.findViewById(R.id.imageButton13);
+            btnName3.setBackgroundResource(R.drawable.abbey_road);
+            btnName3.setLayoutParams(rw);
 
-        btnName4=(ImageButton) activity.findViewById(R.id.imageButton21);
-        btnName4.setBackgroundResource(R.drawable.pink_floyd);
-        btnName4.setLayoutParams(rw);
+            btnName4 =(ImageButton) activity.findViewById(R.id.imageButton21);
+            btnName4.setBackgroundResource(R.drawable.war);
+            btnName4.setLayoutParams(rw);
 
-        listViews.add(btnName1);
-        listViews.add(btnName2);
-        listViews.add(btnName3);
-        listViews.add(btnName4);
+            btnName5 =(ImageButton) activity.findViewById(R.id.imageButton22);
+            btnName5.setBackgroundResource(R.drawable.velvet_underground);
+            btnName5.setLayoutParams(rw);
+
+            btnName6 =(ImageButton) activity.findViewById(R.id.imageButton23);
+            btnName6.setBackgroundResource(R.drawable.unknown_pleasures);
+            btnName6.setLayoutParams(rw);
+
+            btnName7 =(ImageButton) activity.findViewById(R.id.imageButton31);
+            btnName7.setBackgroundResource(R.drawable.sgt_peppers_lonely_hearts_club);
+            btnName7.setLayoutParams(rw);
+
+            btnName8 =(ImageButton) activity.findViewById(R.id.imageButton32);
+            btnName8.setBackgroundResource(R.drawable.rage_against_the_machine);
+            btnName8.setLayoutParams(rw);
+
+            btnName9 =(ImageButton) activity.findViewById(R.id.imageButton33);
+            btnName9.setBackgroundResource(R.drawable.radioactivity);
+            btnName9.setLayoutParams(rw);
+
+            btnName10 =(ImageButton) activity.findViewById(R.id.imageButton41);
+            btnName10.setBackgroundResource(R.drawable.mothership);
+            btnName10.setLayoutParams(rw);
+
+            btnName11 =(ImageButton) activity.findViewById(R.id.imageButton42);
+            btnName11.setBackgroundResource(R.drawable.kid_a);
+            btnName11.setLayoutParams(rw);
+
+            btnName12 =(ImageButton) activity.findViewById(R.id.imageButton43);
+            btnName12.setBackgroundResource(R.drawable.back_in_black);
+            btnName12.setLayoutParams(rw);
+
+            btnName13 =(ImageButton) activity.findViewById(R.id.imageButton51);
+            btnName13.setBackgroundResource(R.drawable.animals);
+            btnName13.setLayoutParams(rw);
+
+            btnName14 =(ImageButton) activity.findViewById(R.id.imageButton52);
+            btnName14.setBackgroundResource(R.drawable.american_idiot);
+            btnName14.setLayoutParams(rw);
+
+            btnName15 =(ImageButton) activity.findViewById(R.id.imageButton53);
+            btnName15.setBackgroundResource(R.drawable.pink_floyd);
+            btnName15.setLayoutParams(rw);
+
+            listViews.add(btnName1);
+            listViews.add(btnName2);
+            listViews.add(btnName3);
+            listViews.add(btnName4);
+            listViews.add(btnName5);
+            listViews.add(btnName6);
+            listViews.add(btnName7);
+            listViews.add(btnName8);
+            listViews.add(btnName9);
+            listViews.add(btnName10);
+            listViews.add(btnName11);
+            listViews.add(btnName12);
+            listViews.add(btnName13);
+            listViews.add(btnName14);
+            listViews.add(btnName15);
+
     }
 
     public ArrayList<?super View> setViews(){
